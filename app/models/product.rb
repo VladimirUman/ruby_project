b5fe72ed32dc3d0305ec52103ee4848ce7cc9c19
Valodya <uva24@mail.ru>
2017-08-18 15:58:16 +0300

    fixed searching

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,7 @@ class Product < ApplicationRecord
 
   def self.search(search)
     if search
-      where('title LIKE ?', "%#{search}%")
+      where('title LIKE ?', '%'+search+'%')
     else
       all
     end
