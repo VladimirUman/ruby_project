ff26b4a23a7db1175c0c61a54544efb590d39afa
Valodya <uva24@mail.ru>
2017-08-25 14:27:37 +0300

    small fixses

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -14,12 +14,15 @@ class Cart < ApplicationRecord
     if current_item
       current_item.quantity += 1
     else
-      current_item = line_items.build(product_id: product.id)      
+      current_item = line_items.build(product_id: product.id)
     end
     current_item
   end
 
   def total_price
     line_items.to_a.sum { |item| item.total_price }
+  end
+  def total_quantity
+    line_items.to_a.sum { |item| item.quantity }
   end
 end
