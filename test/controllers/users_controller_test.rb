4b99fdf41d100902b95cde782cc0b27a7130cbd7
VladimirUman <valodya2008@gmail.com>
2019-05-03 13:48:42 +0300

    Fixed tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -26,7 +26,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should create user" do
     assert_difference('User.count') do
       post users_url, params: { user: { name: 'sam',
-      password: 'secret', password_confirmation: 'secret' } }
+      password: 'secret', password_confirmation: 'secret', email: 'sam@example.com' } }
     end
 
     assert_redirected_to users_url
@@ -44,7 +44,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should update user" do
     patch user_url(@user), params: { user: { name: @user.name,
-      password: 'secret', password_confirmation: 'secret' } }
+      password: 'secret', password_confirmation: 'secret', email: 'sam@example.com' } }
     assert_redirected_to users_url
   end
 
